96df2709019f1db70184c7d8bedbb8dd873dcc0f
javad <javadkavian8@gmail.com>
2023-10-18 21:55:52 +0330

    picture changed to binary succefully

--- p1.m
+++ p1.m
@@ -13,14 +13,23 @@ picture=mygrayfun(picture);
 figure
 subplot(1,2,1)
 imshow(picture)
+ther = find_ther(picture) - 20;
+picture = ~mybinaryfun(picture, ther);
+subplot(1,2,2)
+imshow(picture)
 
-
-function gray_image = mygrayfun(rgb_image)
-    % Extract the red, green, and blue color channels.
-    red_channel = rgb_image(:, :, 1);
-    green_channel = rgb_image(:, :, 2);
-    blue_channel = rgb_image(:, :, 3);
-
-    % Calculate the grayscale image using the formula you provided.
+function gray_image = mygrayfun(image)
+    red_channel = image(:, :, 1);
+    green_channel = image(:, :, 2);
+    blue_channel = image(:, :, 3);
     gray_image = 0.299 * red_channel + 0.578 * green_channel + 0.114 * blue_channel;
 end
+
+function binary_image = mybinaryfun(gray_image, threshold)
+    binary_image = gray_image > threshold;
+end
+
+function thereshold = find_ther(image)
+    thereshold = (min(image) + max(image)) / 2;
+end    
+
